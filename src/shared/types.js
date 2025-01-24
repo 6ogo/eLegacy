1f721c5026192813c9154449e6aab454f15853cd
George <106427405+6ogo@users.noreply.github.com>
2025-01-24 15:35:44 +0100

    Create types.js

---
+++ src/shared/types.js
@@ -0,0 +1,34 @@
+export const GamePhases = {
+    SETUP: 'setup',
+    RESOURCE: 'resource',
+    ACTION: 'action',
+    FORTIFICATION: 'fortification'
+  };
+  
+  export const ActionTypes = {
+    MOVE_UNITS: 'MOVE_UNITS',
+    ATTACK: 'ATTACK',
+    BUILD: 'BUILD',
+    COLLECT_RESOURCES: 'COLLECT_RESOURCES',
+    END_TURN: 'END_TURN',
+    USE_ABILITY: 'USE_ABILITY'
+  };
+  
+  export const ResourceTypes = {
+    GOLD: 'gold',
+    INFLUENCE: 'influence',
+    POWER: 'power'
+  };
+  
+  export const BuildingTypes = {
+    FORTRESS: 'fortress',
+    MARKET: 'market',
+    TEMPLE: 'temple',
+    BARRACKS: 'barracks'
+  };
+  
+  export const UnitTypes = {
+    INFANTRY: 'infantry',
+    CAVALRY: 'cavalry',
+    ARTILLERY: 'artillery'
+  };
